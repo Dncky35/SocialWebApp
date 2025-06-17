70c4bf06d408073233934067a7df6458ad5f988c
Dncky35 <104506895+Dncky35@users.noreply.github.com>
2025-06-17 19:18:10 +0300

    fix: typing issues

--- server/app/core/oauth2.py
+++ server/app/core/oauth2.py
@@ -1,8 +1,9 @@
-from fastapi import Cookie, Request, Depends, HTTPException
+from fastapi import Cookie, status, HTTPException
 from jose import jwt, JWTError, ExpiredSignatureError
 from fastapi.security import OAuth2PasswordBearer
 from datetime import datetime, timedelta
 from .config import settings
+from ..core import database
 from bson import ObjectId
 from ..schemas import token as token_schema
 
@@ -22,11 +23,11 @@ def create_token(data: dict, is_access_token: bool = False):
         "role": data.get("role", "user")
         })
 
-    secret_key = settings.secret_key if is_access_token else settings.refresh_secret_key
+    secret_key = settings.secret_key if is_access_token else settings.secret_key_refresh
     return jwt.encode(to_encode, secret_key, algorithm=settings.algorithm)
 
 def verify_token(token: str, is_access_token: bool = False) -> token_schema.TokenData:
-    secret_key = settings.secret_key_refresh if token_type == "refresh" else settings.secret_key
+    secret_key = settings.secret_key_refresh if not is_access_token else settings.secret_key
     try:
         payload = jwt.decode(token, secret_key, algorithms=[settings.algorithm])
         id: str = payload.get("sub")
@@ -35,7 +36,7 @@ def verify_token(token: str, is_access_token: bool = False) -> token_schema.Toke
         if id is None:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token payload")
         
-        return token_schema.TokenData(account_id=account_id, role=role)
+        return token_schema.TokenData(account_id=id, role=role)
 
     except ExpiredSignatureError:
         raise HTTPException(status_code=401, detail="Token has expired")
@@ -48,7 +49,7 @@ async def get_current_user(token = Cookie(None, alias="accessToken")):
     if not token:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Access token missing")
     token_data = verify_token(token=token, is_access_token=True)
-    account = await database.accounts_collection.find_one({"_id":ObjectId(token.account_id)})
+    account = await database.accounts_collection.find_one({"_id":ObjectId(token_data.account_id)})
     if account is None:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found")
     return account
@@ -58,7 +59,7 @@ async def get_logged_in_user(token = Cookie(None, alias="refreshToken")):
     if not token:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Refresh token missing")
     token_data = verify_token(token=token)
-    account = await database.accounts_collection.find_one({"_id":ObjectId(token.account_id)})
+    account = await database.accounts_collection.find_one({"_id":ObjectId(token_data.account_id)})
     if account is None:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found")
     return account
