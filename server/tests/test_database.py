993e7e59ec57d71aedb56d5b4182222799e35289
Ercan Dinckaya <ercan.dinckaya@gmail.com>
2025-06-18 11:34:56 +0000

    feat: added test for db_name due to env

--- server/tests/test_database.py
+++ server/tests/test_database.py
@@ -1,5 +1,6 @@
 import pytest
 from app.core import database
+from app.core import config
 
 @pytest.mark.asyncio
 async def test_init_db_calls_init_beanie(patch_motor_client_and_init_beanie):
@@ -18,3 +19,33 @@ async def test_init_db_calls_init_beanie(patch_motor_client_and_init_beanie):
     
     # document_models param should include your Account model (by name check)
     assert any("Account" in str(model) for model in called_kwargs.get("document_models", []))
+
+def test_testing_db_name(monkeypatch):
+    monkeypatch.setenv("ENV_FILE", ".env.test")  # production env
+    
+    import importlib
+    importlib.reload(config)
+    importlib.reload(database)
+    
+    assert database.DB_NAME == "social_webapp_test"
+    assert database.db.name == "social_webapp_test"
+
+def test_developing_db_name(monkeypatch):
+    monkeypatch.setenv("ENV_FILE", ".env.dev")  # production env
+    
+    import importlib
+    importlib.reload(config)
+    importlib.reload(database)
+    
+    assert database.DB_NAME == "social_webapp_dev"
+    assert database.db.name == "social_webapp_dev"
+
+def test_production_db_name(monkeypatch):
+    monkeypatch.setenv("ENV_FILE", ".env.prod")  # production env
+    
+    import importlib
+    importlib.reload(config)
+    importlib.reload(database)
+    
+    assert database.DB_NAME == "social_webapp"
+    assert database.db.name == "social_webapp"
