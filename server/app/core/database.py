5449ddc6e6220772c6aab69304a9771414138f7f
Dncky35 <104506895+Dncky35@users.noreply.github.com>
2025-06-18 18:45:35 +0300

    merged

--- server/app/core/database.py
+++ server/app/core/database.py
@@ -1,12 +1,17 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from .config import settings
+from dotenv import load_dotenv
 import os
+
+# Load correct env file before anything else
+env_file = os.getenv("ENV_FILE", ".env.dev")
+load_dotenv(env_file)
 
 MONGO_URI=f"mongodb+srv://{settings.mongodb_username}:{settings.mongodb_password}@cluster0.yu7sul0.mongodb.net/"
 client = AsyncIOMotorClient(MONGO_URI)
 
 # 🧪 allow test override
-DB_NAME = os.getenv("DB_NAME", "social_webapp")
+DB_NAME = os.getenv("DB_NAME")
 db = client[DB_NAME]
 account_collection = db["accounts"]
 
