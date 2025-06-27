44ae94eef8076102ddd2d8575299db8be2beaf2f
Ercan Dinckaya <ercan.dinckaya@gmail.com>
2025-06-27 11:45:05 +0000

    feat: added test for login

--- server/tests/integration/test_auth_login.py
+++ server/tests/integration/test_auth_login.py
@@ -9,8 +9,52 @@ async def test_login_success(client):
         "password": "securepassword",
         "full_name": "Test User"
     }
-    response = await client.post("/auth/signup", data=payload)
-    assert response.status_code == 200
+    response_create = await client.post("/auth/signup", data=payload)
+    assert response_create.status_code == 200
 
+    login_payload={
+        "username": "testuser@example.com",
+        "password": "securepassword"
+    }
+
+    response_login = await client.post("/auth/login", data=login_payload)
+    assert response_login.status_code == 202
     
+@pytest.mark.asyncio
+async def test_login_invalid_credentials(client):
+    # create a test account
+    payload = {
+        "email": "testuser2@example.com",
+        "username": "testuser2",
+        "password": "securepassword",
+        "full_name": "Test User"
+    }
+    response_create = await client.post("/auth/signup", data=payload)
+    assert response_create.status_code == 200
 
+    # wrong username
+    login_payload={
+        "username": "testuser@example.com",
+        "password": "securepassword"
+    }
+
+    response_login = await client.post("/auth/login", data=login_payload)
+    assert response_login.status_code == 401
+
+    #wrong password 
+    login_payload={
+        "username": "testuser2@example.com",
+        "password": "securepassword2"
+    }
+
+    response_login = await client.post("/auth/login", data=login_payload)
+    assert response_login.status_code == 401
+
+    #wrong password wrong username
+    login_payload={
+        "username": "testuser@example.com",
+        "password": "securepassword2"
+    }
+
+    response_login = await client.post("/auth/login", data=login_payload)
+    assert response_login.status_code == 401
