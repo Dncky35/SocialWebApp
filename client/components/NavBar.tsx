2fb9cd17b8a8b42a7fab6c24e20c461c7272e14d
Dncky35 <104506895+Dncky35@users.noreply.github.com>
2025-07-22 19:18:10 +0300

    fix: navbar

--- client/components/NavBar.tsx
+++ client/components/NavBar.tsx
@@ -4,7 +4,7 @@ const NavBar: React.FC = () => {
     return (
         <div className="w-full bg-cyan-950 p-4">
             <div className="container ">
-                <Link href={"/"}>Icon</Link>
+                <Link href={"/"}>✈</Link>
             </div>
             <div>
             
