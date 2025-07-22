7777da75593a4bcd8827373faa915c7bc4a49947
Ercan Dinckaya <ercan.dinckaya@gmail.com>
2025-07-22 09:45:02 +0000

    chore: remove unused favicon and SVG assets, clean up global CSS
    - Deleted favicon.ico and window.svg as they are no longer in use
    - Removed unused CSS custom properties from globals.css

---
+++ client/components/Footer.tsx
@@ -0,0 +1,13 @@
+
+const Footer:React.FC = () => {
+    return (
+        <footer className="bg-teal-950 text-white py-4">
+            <div className="flex items-center justify-between md:flex-row px-4">
+                <p className="text-lg font-semibold mb-4 md:mb-0">© 2025 CloudROcean</p>
+                <div></div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ client/components/NavBar.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+const NavBar: React.FC = () => {
+    return (
+        <div className="w-full bg-teal-950 p-4">
+            <div className="container ">
+                <Link href={"/"}>Icon</Link>
+            </div>
+            <div>
+            
+            </div>
+        </div>
+    );
+};
+
+export default NavBar;
