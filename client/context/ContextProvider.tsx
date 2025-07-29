889b688894c4491cd25e2944f909a98d1b7c65d2
Dncky35 <104506895+Dncky35@users.noreply.github.com>
2025-07-29 19:01:03 +0300

    hotfix: url problem

--- client/context/ContextProvider.tsx
+++ client/context/ContextProvider.tsx
@@ -2,7 +2,8 @@
 import { AuthProvider } from "./AuthContext";
 import { PostProvider } from "./PostContext";
 
-export const BASEURL = process.env.BACKEND_URL ? "/api/" : "https://8000-dncky35-socialwebapp-09hw3b6xdaz.ws-us120.gitpod.io/"
+// export const BASEURL = process.env.BACKEND_URL ? "/api/" : "https://8000-dncky35-socialwebapp-09hw3b6xdaz.ws-us120.gitpod.io/"
+export const BASEURL = "/api/" 
 
 export const AppProviders = ({children}: {children: React.ReactNode}) => {
     return (
