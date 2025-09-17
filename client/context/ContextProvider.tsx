53380a64d5e8caa506dd2bd476cec28ab6e4c61f
Ercan Dinckaya <ercan.dinckaya@gmail.com>
2025-09-17 07:12:59 +0000

    Editing Posts

--- client/context/ContextProvider.tsx
+++ client/context/ContextProvider.tsx
@@ -3,6 +3,7 @@ import { AuthProvider } from "./AuthContext";
 import { PostProvider } from "./PostContext";
 
 // export const BASEURL = "https://8000-dncky35-socialwebapp-09hw3b6xdaz.ws-us121.gitpod.io/"
+
 export const BASEURL = "/api/" 
 
 export const AppProviders = ({children}: {children: React.ReactNode}) => {
