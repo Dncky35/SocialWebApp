95b168a56115e1b5a987a4d7fef36f0dbbe7f633
Dncky35 <ercan.dinckaya@gmail.com>
2025-11-04 12:55:44 +0000

    .

--- client/context/ContextProvider.tsx
+++ client/context/ContextProvider.tsx
@@ -4,8 +4,8 @@ import { AuthProvider } from "./AuthContext";
 import { PostProvider } from "./PostContext";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
-export const BASEURL = "https://8000-cs-557130547078-default.cs-europe-west1-haha.cloudshell.dev/";
-// export const BASEURL = "/api/";
+// export const BASEURL = "https://8000-cs-557130547078-default.cs-europe-west1-haha.cloudshell.dev/";
+export const BASEURL = "/api/";
 const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || "";
  
 export const AppProviders = ({ children }: { children: React.ReactNode }) => {
