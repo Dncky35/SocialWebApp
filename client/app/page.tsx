2879a9a00ef46c9b438b19eb43e89347467e8082
Dncky35 <ercan.dinckaya@gmail.com>
2025-11-04 11:27:53 +0000

    .

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,19 +3,20 @@ import GoogleLoginButton from "@/components/GoogleLoginButton";
 import { useAuth } from "@/context/AuthContext";
 import Link from "next/link";
 
-const RootPage:React.FC = () => {
+const RootPage: React.FC = () => {
   const { account } = useAuth();
 
   return (
     <div>
-      { account ? null : (
+      {account ? null : (
         <div className="fixed inset-0 flex justify-center items-center bg-black/80 z-50">
           <div className="p-4 rounded shadow-xl">
             <h1 className="text-center font-bold text-2xl mt-2">You haven&apos;t Signin yet</h1>
             <div className="mt-4 flex flex-col items-center justify-center space-y-4 bg-slate-700/50 py-4 rounded-lg">
-              <div className="flex items-center justify-center">
+              <div className="flex-cols items-center justify-center">
+                <h2 className="text-center mb-4 font-semibold text-lg border-b ">Sign in with</h2>
                 <GoogleLoginButton />
-            </div>
+              </div>
             </div>
             <div className="mt-4 text-center">
               <p>
